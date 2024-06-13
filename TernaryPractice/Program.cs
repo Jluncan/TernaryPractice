d2d051660afb47b773bb7c8a39749b2a2b8c8420
James Luncan <jluncan@tql.com>
2024-06-13 14:12:15 -0400

    Add project files.

---
+++ TernaryPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace TernaryPractice {
+    internal class Program {
+        static void Main(string[] args) {
+
+            for (var i = 1; i <= 30; i++) {
+
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.Console.WriteLine($"{i} : FIZZBUZZ");
+                } else if (i % 3 == 0) {
+                    System.Console.WriteLine($"{i} : FIZZ");
+                } else if (i % 5 == 0) {
+                    System.Console.WriteLine($"{i} : BUZZ");
+                } else {
+                    System.Console.WriteLine($"{i}");
+                }
+
+            }
+        }
+
+    }
+}
+    
+
